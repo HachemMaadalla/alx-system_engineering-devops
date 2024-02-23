8c91b92cf5889e7d816d725f935322b9e548bc6f
HachemMaadalla <138704463+HachemMaadalla@users.noreply.github.com>
2024-02-23 16:33:51 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,8 @@
-# Installs puppet-lint
+package { 'python3-pip':
+  ensure => installed,
+}
 
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  unless  => '/usr/bin/pip3 show flask | grep Version | grep 2.1.0',
 }
